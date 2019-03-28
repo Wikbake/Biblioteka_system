703ad6f1284e69458e0d57b5a9d22503a5ed3dce
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-03-28 22:37:47 +0100

    Added connection with a database

--- src/Biblioteka/Biblioteka_GUI.java
+++ src/Biblioteka/Biblioteka_GUI.java
@@ -4,10 +4,60 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Biblioteka_GUI extends JFrame {
 
+
+
   public Biblioteka_GUI() {
+
+    try {
+      connection = DriverManager.getConnection("jdbc:postgresql:Data_Lib", "postgres", "postgres");
+
+      Statement statement = connection.createStatement();
+      statement.setQueryTimeout(5);
+      statement.executeUpdate("CREATE TABLE IF NOT EXISTS Books(" +
+              "Book_Id serial PRIMARY KEY, " +
+              "Book_name VARCHAR(50) NOT NULL, " +
+              "Cost INTEGER NOT NULL, " +
+              "YearOfPublishment INTEGER NOT NULL, " +
+              "Genre VARCHAR(50) NOT NULL, " +
+              "Author VARCHAR(50) NOT NULL, " +
+              "Age INTEGER NOT NULL, " +
+              "Availability VARCHAR(5) NOT NULL)");
+      statement.executeUpdate("CREATE TABLE IF NOT EXISTS Customers(" +
+              "Customer_Id serial PRIMARY KEY, " +
+              "Surname VARCHAR(50) NOT NULL, " +
+              "Name VARCHAR(50) NOT NULL, " +
+              "Adress VARCHAR(255) NOT NULL, " +
+              "Phone INTEGER NOT NULL)");
+      statement.executeUpdate("CREATE TABLE IF NOT EXISTS Rented_books(" +
+              "Rental_Id serial PRIMARY KEY, " +
+              "Book_Id INTEGER NOT NULL, " +
+              "Customer_Id INTEGER NOT NULL, " +
+              "Rental_date timestamp without time zone NOT NULL, " +
+              "Book_name VARCHAR(50) NOT NULL, " +
+              "Return_date timestamp without time zone NOT NULL, " +
+              "CONSTRAINT rented_books_book_id_fkey FOREIGN KEY (Book_Id) REFERENCES Books (Book_Id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, " +
+              "CONSTRAINT rented_books_customer_id_fkey FOREIGN KEY (Customer_Id) REFERENCES Customers (Customer_Id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION)");
+      statement.executeUpdate("CREATE TABLE IF NOT EXISTS Returns(" +
+              "Rental_Id INTEGER PRIMARY KEY, " +
+              "Customer_Id INTEGER NOT NULL, " +
+              "Book_name VARCHAR(50) NOT NULL, " +
+              "Return_date timestamp without time zone NOT NULL, " +
+              "CONSTRAINT returns_rental_id_fkey FOREIGN KEY (Rental_Id) REFERENCES Rented_books (Rental_Id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION)");
+    } catch (SQLException ex) {
+      Logger.getLogger(Biblioteka_GUI.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+
+
     add(Library);
     this.setTitle("Library");
     this.setBounds(0, 0, 250, 250);
@@ -20,6 +70,9 @@ public class Biblioteka_GUI extends JFrame {
       }
     });
   }
+
+  Connection connection;
+
 
   private void addBookActionPerformed(ActionEvent e) {
     System.out.println("Hello!"); // do zmodyfikowania
@@ -48,7 +101,7 @@ public class Biblioteka_GUI extends JFrame {
   private JList rentalList;
   private JList returnList;
   private JTextField yyyyMmDdTextField;
-  private JButton button1;
+  private JButton addReturnButton;
 
   public static void main(String[] args) {
     EventQueue.invokeLater(new Runnable() {
