986c18aaea2018a824600d19fe3cccdd41180d0f
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-03-31 20:28:07 +0200

    Changed name of a class, added new hashCode in Rental class.

---
+++ src/Biblioteka/Rental.java
@@ -0,0 +1,37 @@
+package Biblioteka;
+
+import java.util.Date;
+
+public class Rental {
+
+  private int idRental;
+  private int idBook;
+  private String bookName;
+  private Date dateReturn;
+  private Date dateRental;
+
+  public int getIdRental() { return idRental; }
+  public int getIdBook() { return idBook; }
+  public Date getDateRental() { return dateRental; }
+  public Date getDateReturn() { return dateReturn; }
+  public String getBookName() {return bookName; }
+
+  public void setIdRental(int idRental) { this.idRental = idRental; }
+  public void setIdBook(int idBook) { this.idBook = idBook; }
+  public void setBookName(String bookName) { this.bookName = bookName; }
+  public void setDateReturn(Date dateReturn) { this.dateReturn = dateReturn; }
+  public void setDateRetnal(Date dateRental) { this.dateRental = dateRental; }
+
+  public Rental(int idRental, int idBook, String bookName, Date dateReturn, Date dateRental) {
+    this.idRental = idRental;
+    this.idBook = idBook;
+    this.bookName = bookName;
+    this.dateReturn = dateReturn;
+    this.dateRental = dateRental;
+  }
+
+  @Override
+  public int hashCode() {
+    return bookName.hashCode();
+  }
+}
--- src/Biblioteka/Wypozyczenie.java
+++
@@ -1,6 +0,0 @@
-package Biblioteka;
-
-public class Wypozyczenie {
-
-
-}
