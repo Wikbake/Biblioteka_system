4ea4236150a444886845657ab2a416766e2ee6e1
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-03 15:08:29 +0200

    New class which supports rentals

---
+++ src/Biblioteka/Return.java
@@ -0,0 +1,22 @@
+package Biblioteka;
+
+import java.util.Date;
+
+public class Return extends Rental{
+
+    private Date dateReturnCustomer;
+
+    public Date getDateReturnCustomer() { return dateReturnCustomer; }
+
+    public void setDateReturnCustomer(Date dateReturnCustomer) { this.dateReturnCustomer = dateReturnCustomer; }
+
+    public Return(int idRental, int idBook, String bookName, Date dateReturn, Date dateRental, Date dateReturnCustomer) {
+        super(idRental, idBook, bookName, dateReturn, dateRental);
+        this.dateReturnCustomer = dateReturnCustomer;
+    }
+
+    @Override
+    public int hashCode() {
+        return bookName.hashCode();
+    }
+}
